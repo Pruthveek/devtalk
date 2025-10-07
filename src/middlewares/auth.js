5bbf4c7b76534fb19e9df4ced8044ebff3e278ae
Pruthveek <pruthveek512@gmail.com>
2025-10-07 14:57:20 +0530

    done

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -5,15 +5,18 @@ const userAuth = async (req, res, next) => {
   try {
     const { token } = req.cookies;
     if (!token) {
-      return res.status(401).send("Please login!")
+      return res.status(401).send("Please Login!");
     }
-    const decodedObj = await jwt.verify(token, process.env.JWT_SECRET_KEY );
+
+    const decodedObj = jwt.verify(token, process.env.JWT_SECRET);
+
     const { _id } = decodedObj;
+
     const user = await User.findById(_id);
     if (!user) {
-      throw new Error("User does not exist");
+      throw new Error("User not found");
     }
-    console.log("Authenticated User:", user);
+
     req.user = user;
     next();
   } catch (err) {
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,84 +1,72 @@
 const express = require("express");
 const authRouter = express.Router();
-const { validateSigninData } = require("../utils/validation");
-const bcrypt = require("bcrypt");
-const validator = require("validator");
+
+const { validateSignUpData } = require("../utils/validation");
 const User = require("../models/user");
+const bcrypt = require("bcrypt");
 
-
-authRouter.post("/signin", async (req, res) => {
+authRouter.post("/signup", async (req, res) => {
   try {
-    validateSigninData(req);
+    // Validation of data
+    validateSignUpData(req);
+
     const { firstName, lastName, email, password } = req.body;
+
+    // Encrypt the password
     const passwordHash = await bcrypt.hash(password, 10);
+    console.log(passwordHash);
+
+    //   Creating a new instance of the User model
     const user = new User({
       firstName,
       lastName,
       email,
       password: passwordHash,
     });
+
     const savedUser = await user.save();
-    const token = await user.getJWT();
+    const token = await savedUser.getJWT();
 
-    const cookieOptions = {
-      expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
-      httpOnly: true,
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-      secure: process.env.NODE_ENV === "production",
-    };
+    res.cookie("token", token, {
+      expires: new Date(Date.now() + 8 * 3600000),
+    });
 
-    res.cookie("token", token, cookieOptions);
-
-    res.json({ message: "Sign-up sucessfully", data: savedUser });
+    res.json({ message: "User Added successfully!", data: savedUser });
   } catch (err) {
-    res
-      .status(400)
-      .json({ message: "Something went wrong", error: err.message });
+    res.status(400).send("ERROR : " + err.message);
   }
 });
 
 authRouter.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
-    if (!validator.isEmail(email)) {
-      throw new Error("Invalid credentials");
-    }
+
     const user = await User.findOne({ email: email });
     if (!user) {
       throw new Error("Invalid credentials");
     }
     const isPasswordValid = await user.validatePassword(password);
-    if (!isPasswordValid) {
-      throw new Error("Invalid credentials");
-    } else {
-      // create a jwt token
+
+    if (isPasswordValid) {
       const token = await user.getJWT();
 
-      const cookieOptions = {
-        expires: new Date(Date.now() + 1 * 24 * 60 * 60 * 1000),
-        httpOnly: true,
-        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-        secure: process.env.NODE_ENV === "production",
-      };
-
-      res.cookie("token", token, cookieOptions);
+      res.cookie("token", token, {
+        expires: new Date(Date.now() + 8 * 3600000),
+      });
       res.send(user);
+    } else {
+      throw new Error("Invalid credentials");
     }
   } catch (err) {
-    res
-      .status(400)
-      .json({ message: "Something went wrong", error: err.message });
+    res.status(400).send("ERROR : " + err.message);
   }
 });
 
 authRouter.post("/logout", async (req, res) => {
-  const cookieOptions = {
-    httpOnly: true,
-    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-    secure: process.env.NODE_ENV === "production",
-    expires: new Date(0),
-  };
-  res.cookie("token", "", cookieOptions).send("User logged out");
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+  });
+  res.send("Logout Successful!!");
 });
 
 module.exports = authRouter;
