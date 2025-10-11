e1c5f075c7343d565d1b19109fac26d90deb077a
Pruthveek <pruthveek512@gmail.com>
2025-10-11 10:04:59 +0530

    db

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,14 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  await mongoose.connect(process.env.MONGO_URI);
+  try {
+    await mongoose.connect(process.env.MONGO_URI);   
+  } catch (err) {
+    res
+      .status(400)
+      .json({ message: "Something went wrong", error: err.message });
+  }
+  
 };
 
 module.exports = connectDB;
